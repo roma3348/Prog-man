dc132bb6017c154a639646c0a9808a45341618db
Saint Jakuzi <saint3543jakuzi@gmail.com>
2015-08-14 19:55:08 +0300

    Update score.js

--- JS/Scenes/score.js
+++ JS/Scenes/score.js
@@ -1,14 +1,12 @@
 Crafty.scene("Score", function() {
 	Crafty.background("#000");
-    Crafty.e("2D, DOM, Text").attr({w: 100, h: 20, x: 150, y: 120})
-    .text("The end. <br/>Your score: " + Inc.score)
+    	Crafty.e("2D, DOM, Text").attr({w: 100, h: 20, x: 150, y: 120})
+    	.text("You died. <br/>Your score: " + Inc.score)
 	.css({"text-align": "center"});
-	Inc.score = 0;
-	Inc.lives = 3;
-    /*window.onclick(function() {
+    	/*window.onclick(function() {
 		Crafty.scene("main");
 	})*/
 	setTimeout(function() {
-        Crafty.scene("main");
-    }, 3000);
+        	Crafty.scene("game");
+    	}, 5000);
 });
